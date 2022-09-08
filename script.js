c4ca7e45d1082204b6b17ebe8748647d51162a1c
Sina <bekarsina@gmail.com>
2022-09-08 11:01:33 +0430

    finaly comment for this modal

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-let product = {
-  title: "",
-  description: "",
-  discountPrice: "",
-  mainPrice: "",
-  img: "",
-};
-
 let modal = document.querySelector(".conteiner");
 let closeModalBtn = document.querySelector(".close-modal");
 let skipModal = document.querySelector(".skip-modal");
+let btn = document.querySelector(".btn");
+let section = document.querySelector(".section");
 
 
 skipModal.addEventListener('click', closeModal)
 
 closeModalBtn.addEventListener('click', closeModal)
 
+btn.addEventListener('click', () => {
+  modal.classList.remove('hidden')
+  modal.classList.add('block')
+  // section.classList.add('blur-sm')
+
+})
+
 function closeModal(){
-  modal.classList.add('hidden')
+  modal.classList.add('hidden');
+  modal.classList.remove('block')
+  // section.classList.remove('blur-sm')
 }
